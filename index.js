70d136cd2b5ac1cd6ff81fd9cd7ed063b0796f25
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-04-23 18:11:50 +0600

    cors origin added

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "https://sunbeam-kids-garden-client-final.vercel.app/",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // MongoDB Connection URL
